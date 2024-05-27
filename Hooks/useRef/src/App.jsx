ed311c7c072bfeb16cde23a582767c2f2e325fbd
himanshuwiz <himdhi2003@gmail.com>
2024-05-28 02:43:08 +0530

    useRef Hook

---
+++ Hooks/useRef/src/App.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useRef, useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  // const [count, setCount] = useState(0);
+
+  //Using useRef will solve this problem of infinite re-renders so we use it
+  //instead of using a useState hook for counting those re-renders
+  const count = useRef(0);
+  useEffect(() => {
+    count.current = count.current + 1;
+  });
+
+  //This will cause infinite loop of re-renders
+  // useEffect(() => {
+  //   setCount((prev) => prev + 1);
+  // });
+
+  return (
+    <>
+      <input type="text" onChange={(e) => setName(e.target.value)} />
+      <h2>
+        <span>Enter Your Name:</span> {name}
+      </h2>
+      <h2>Renders: {count.c}</h2>
+    </>
+  );
+}
+
+export default App;
